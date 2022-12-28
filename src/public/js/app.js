dde5e3f99aa7cb8bb9006b49d21cb74b1468c286
김민균 <alsrbs4024@gmail.com>
2022-12-28 20:11:02 +0900

    2022.12.28 WebSocket으로 실시간 메세지 보내기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,20 @@
-alert("hello");
+//프론트엔드 JS
+//서버로부터 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+//메세지
+socket.addEventListener("open", () => {
+  console.log("Connected to Server!");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Shotdown to Server!");
+});
+
+setTimeout(() => {
+  socket.send("hello from the Brower");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -6,7 +8,24 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-
+app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log("Listening on http://localhost:3000/!!");
 
-app.listen(3000, handleListen);
+//http와 웹 소켓 서버 연결
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+//socket은 서버와 브라우져의 연결
+wss.on("connection", (socket) => {
+  console.log("Connected to Brower!");
+  socket.on("close", () => {
+    console.log("Shotdown to Brower!");
+  });
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello i'm socket");
+});
+
+//포트 번호 3000
+server.listen(3000, handleListen);
