588215b7842f48beff67c0cb7afb3ed6c6bc20c5
김민균 <alsrbs4024@gmail.com>
2023-01-02 17:37:00 +0900

    2023.01.02 Socket.IO set and rechange

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,50 +1,16 @@
-//front-end 자바 스크립트
-//서버로부터 연결
-const messageList = document.querySelector("ul");
-const nickNameForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
 //function
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-}
-
-function handleOpen() {
-  console.log("Connected to Server!");
-}
-function handleMessage(message) {
-  console.log("New message: ", message.data);
-}
-function handleClose() {
-  console.log("Shotdown to Server!");
-}
-function handleSubmit(event) {
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
-  input.value = "";
-}
-function handleNickSubmit(event) {
-  event.preventDefault();
-  const input = nickNameForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done");
+  });
   input.value = "";
 }
 
-//메세지
-socket.addEventListener("open", handleOpen);
-
-socket.addEventListener("message", handleMessage);
-
-socket.addEventListener("close", handleClose);
-
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-messageForm.addEventListener("submit", handleSubmit);
-nickNameForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 //back-end 자바 스크립트
 import express from "express";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import http from "http";
 
 const app = express();
@@ -10,22 +10,22 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-const handleListen = () => console.log("Listening on http://localhost:3000/!!");
 
 //http와 웹 소켓 서버 연결
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-function onSocketClose() {
-  console.log("Shotdown to Brower!");
-}
-
-function onSocketMessage(message) {
-  console.log(message.toString("utf8"));
-}
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
+});
 
 //socket은 서버와 브라우져의 연결
-const sockets = [];
+/* const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
@@ -47,6 +47,7 @@ wss.on("connection", (socket) => {
     }
   });
 });
-
+*/
 //포트 번호 3000
-server.listen(3000, handleListen);
+const handleListen = () => console.log("Listening on http://localhost:3000/");
+httpServer.listen(3000, handleListen);
