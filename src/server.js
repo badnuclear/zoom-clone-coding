623bddeffefbd0eccfb4bd6893f2595fab04911c
김민균 <alsrbs4024@gmail.com>
2023-01-06 20:30:51 +0900

    2023.01.06 Video call start

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 //back-end 자바 스크립트
 import express from "express";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import SocketIO from "socket.io";
 import http from "http";
 
 const app = express();
@@ -14,85 +13,6 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 //http와 웹 소켓 서버 연결
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io/"],
-    credentials: true,
-  },
-});
-instrument(wsServer, {
-  auth: false,
-});
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(wsServer.sockets.adapter);
-    console.log(`Socket Event: ${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-    socket.on("disconnect", () => {
-      wsServer.sockets.emit("room_change", publicRooms());
-    });
-  });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-//socket은 서버와 브라우져의 연결
-/* const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to Brower!");
-  socket.on("close", onSocketClose);
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(
-            `${socket.nickname}: ${message.payload.toString("utf8")}`
-          )
-        );
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-  });
-});
-*/
-//포트 번호 3000
+const wsServer = SocketIO(httpServer);
 const handleListen = () => console.log("Listening on http://localhost:3000/");
 httpServer.listen(3000, handleListen);
