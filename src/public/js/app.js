d4f7f36fbbb4d40f380bb00e71e61e194ec6f2a6
김민균 <alsrbs4024@gmail.com>
2023-01-10 13:30:47 +0900

    2023.01.10 STUN server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -83,6 +83,13 @@ function handleCameraClick() {
 }
 async function handleCameraChange() {
   await getMedia(cameraSelect.value);
+  const videoTrack = myStream.getVideoTracks()[0];
+  if (myPeerConnection) {
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "vidoe");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -141,9 +148,22 @@ socket.on("ice", (ice) => {
 //RTC Code
 
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
+
   myPeerConnection.addEventListener("icecandidate", handleIce);
-  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myPeerConnection.addEventListener("track", handleTrack);
   myStream
     .getTracks()
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
@@ -152,7 +172,7 @@ function makeConnection() {
 function handleIce(data) {
   socket.emit("ice", data.candidate, roomName);
 }
-function handleAddStream(data) {
+function handleTrack(data) {
   const peerFace = document.getElementById("peerFace");
-  peerFace.srcObject = data.stream;
+  peerFace.srcObject = data.streams[0];
 }
